c57b78c34f3db76b1125daeebcd9cb5e7743f538
Eduardo Schmidt Schultz <schmidt_eduardo@outlook.com>
2023-07-05 10:48:48 -0300

    Start OK

---
+++ TITANIC_game/Assets/scripts/menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public string nomeCena;
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ativando painel do menu
+        menuPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(nomeCena);
+    }
+}
